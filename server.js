be3a096221c6cf899dbe9b913200bc10376dc743
Jean Bispo <jean.bispo@sanar.com>
2020-12-02 20:26:09 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const Twit = require('twit');
+require('dotenv').config();
+
+const Bot = new Twit({
+  consumer_key:         process.env.CONSUMER_KEY,
+  consumer_secret:      process.env.CONSUMER_SECRET,
+  access_token:         process.env.ACCESS_TOKEN,
+  access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
+});
+
+function BotInit() {
+
+  var stream = Bot.stream('statuses/filter', { track: 'imposto'});
+  
+    stream.on('tweet', tweet => {
+        var reply = 'Imposto é roubo';
+        var nameID = tweet.id_str;
+        var name = tweet.user.screen_name;
+        Bot.post('statuses/update', {in_reply_to_status_id: nameID, status: reply + ' @' + name}, () => null);
+    });
+
+}
+
+BotInit();
